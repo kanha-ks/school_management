143da1fc1edcf956ce0ab3c0e8071bf5be697af4
kanha-ks <kanha.namdev@ksolves.com>
2026-02-20 19:01:28 +0530

    successfully added all the updated files

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details
+
 from . import student
 from . import teacher
 from . import subjects
 from . import classes
 from . import result
-from . import sale_order
